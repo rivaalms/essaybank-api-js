097fff19c618a04f48bbc1dbafcad3f2f0bb46e6
Riva Almero <riva@dev.netkromsolution.com>
2025-06-04 13:25:29 +0700

    feat: crud responses

---
+++ src/app/controllers/responses.js
@@ -0,0 +1,75 @@
+const { paginate, parseResponse } = require("../helpers/http")
+const { Response } = require("../models")
+
+module.exports = {
+   async get(req, res) {
+      const data = await paginate(Response, req.query.page, req.query.perPage)
+      res.json(parseResponse({ data }))
+   },
+   async find(req, res) {
+      const data = await Response.findByPk(req.params.id)
+      res.json(parseResponse({ data }))
+   },
+   async create(req, res) {
+      const payload = {
+         questionId: req.body.questionId,
+         responseText: req.body.responseText,
+         identifier: req.ip,
+      }
+
+      try {
+         const result = await Response.create(payload)
+         res.json(
+            parseResponse({
+               data: result,
+               message: "Response created successfully",
+            })
+         )
+      } catch (e) {
+         res.json(
+            parseResponse({
+               data: e.toString(),
+               error: "Failed to create question",
+            })
+         )
+      }
+   },
+   async update(req, res) {
+      const model = await Response.findByPk(req.params.id)
+      const payload = {
+         questionId: req.body.questionId,
+         responseText: req.body.responseText,
+      }
+
+      try {
+         const result = await model.update(payload)
+         res.json(
+            parseResponse({
+               data: result,
+               message: "Response updated successfully",
+            })
+         )
+      } catch (e) {
+         res.json(
+            parseResponse({
+               data: e.toString(),
+               error: "Failed to update question",
+            })
+         )
+      }
+   },
+   async destroy(req, res) {
+      const model = await Response.findByPk(req.params.id)
+      try {
+         await model.destroy()
+         res.json(parseResponse({ message: "Response deleted successfully" }))
+      } catch (e) {
+         res.json(
+            parseResponse({
+               data: e.toString(),
+               error: "Failed to delete question",
+            })
+         )
+      }
+   },
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 const express = require("express")
+const { parseResponse } = require("../app/helpers/http")
 const userRouter = require("./api/users")
 const questionRouter = require("./api/questions")
+const responseRouter = require("./api/responses")
 
 const router = express.Router({ mergeParams: true })
 
@@ -9,7 +11,7 @@ router.use(express.urlencoded({ extended: true }))
 
 router.use((req, res, next) => {
    if (req.header("accept") !== "application/json") {
-      res.status(406).json({ error: "Not acceptable" })
+      res.status(406).json(parseResponse({ error: "Not acceptable" }))
       return
    }
    next()
@@ -17,5 +19,6 @@ router.use((req, res, next) => {
 
 router.use("/users", userRouter)
 router.use("/questions", questionRouter)
+router.use("/responses", responseRouter)
 
 module.exports = router
