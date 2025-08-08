36ce621f602aeed395709a76fc6d1026248fa438
Riva Almero <rvalms@icloud.com>
2025-08-08 13:23:39 +0700

    fix: undefined fn

--- src/app/controllers/auth.js
+++ src/app/controllers/auth.js
@@ -10,7 +10,7 @@ module.exports = {
          })
          if (!user) {
             res.status(401).json(
-               resolveResponse({
+               parseResponse({
                   error: "Your credentials does not match our records",
                })
             )
