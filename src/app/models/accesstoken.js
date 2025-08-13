49a9a0c879562f853cab712d7592616b2fc8a014
Riva Almero <rvalms@icloud.com>
2025-08-13 17:47:18 +0700

    fix: token generation expiresAt default value

--- src/app/models/accesstoken.js
+++ src/app/models/accesstoken.js
@@ -28,7 +28,7 @@ module.exports = (sequelize, DataTypes) => {
          expiresAt: {
             allowNull: false,
             type: DataTypes.DATE,
-            defaultValue: new Date(Date.now() + 1000 * 60 * 60 * 24) // 1 day,
+            defaultValue: DataTypes.NOW
          },
       },
       {
